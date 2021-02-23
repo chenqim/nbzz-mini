0c2fd996310eca1f345424a85b6e2c524db8b015
sunsj <sunsj@singzy.com>
2021-02-23 13:48:37 +0800

    修改api地址

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,5 @@
 module.exports = {
   // apiPrefix:"https://kycloud.singzy.com:18443/api/",
-  apiPrefix:"https://wxyjd.fixme.icu:18443/api/",
+  // apiPrefix:"https://wxyjd.fixme.icu:18443/api/",
+  apiPrefix:"https://www.wxyjd.cn:18443/api/",
 }
